d3f08c4992774ca738ccfbdfcb645899cffeed97
Nandkishor Dhakad <151372772+nkdkd@users.noreply.github.com>
2024-08-12 01:46:31 +0530

    Update taskServices.js

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,18 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
+
 const apiUrl = 'http://k8s-threetie-mainlb-11c5700e30-1598476283.us-east-1.elb.amazonaws.com';
-console.log(apiUrl)
+
+const api = axios.create({
+    baseURL: apiUrl,
+});
+
 export function getTasks() {
-    return axios.get(apiUrl);
+    return api.get('/api/tasks');
 }
 
 export function addTask(task) {
-    return axios.post(apiUrl, task);
+    return api.post('/api/tasks', task);
 }
 
 export function updateTask(id, task) {
-    return axios.put(apiUrl + "/" + id, task);
+    return api.put(`/api/tasks/${id}`, task);
 }
 
 export function deleteTask(id) {
-    return axios.delete(apiUrl + "/" + id);
+    return api.delete(`/api/tasks/${id}`);
 }
